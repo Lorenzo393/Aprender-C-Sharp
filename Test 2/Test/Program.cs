291fb15cc97fbc9d2a0d31eeffd222b170e1ae23
Lorenzo393 <loloc.gelos@outlook.com>
2025-05-15 11:22:48 -0300

    Agrego test 2

---
+++ Test 2/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(suma(1,2));
+            int age = 10;
+            add(age);
+            Console.WriteLine(age);
+        }
+
+        static int suma(int x, int y)
+        {
+            return x + y;
+        }
+        static int suma(int x, int y, int z)// Esto no funciona con la plantilla basica de C#
+        {
+            return x + y;
+        }
+        static int add(int x)
+        {
+            return x++;
+        }
+    }
+}
---
+++ Test/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age = 28;
+string diablo = "diablo";
+
+Console.WriteLine("Hello World");
+Console.WriteLine("El " + diablo + " tiene " + age + " años de edad");
+Console.WriteLine($"El {diablo} tiene {age} años de edad"); // Dos formas de hacerlo
+Console.WriteLine($"El \"{diablo}\" tiene {age} años de edad");
+Console.WriteLine(suma(1,2));
+
+var var1 = "hola";
+var var2 = 10;
+var var3 = 10.4f;// Como en python
+
+// Convert.
+
+
+static int suma(int a, int b){
+    return a + b; 
+}
